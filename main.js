9f09003e0ca472bf9b306459467cf6061e0da40b
Raferto <rafid.ferdianto@gmail.com>
2020-10-15 08:57:08 +0700

    Create Rectangle - Triangle_FAN

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ function main() {
   var vertices = [
     -0.5, 0.5,      // Titik A
     0.5, 0.5,       // Titik B
-    0.5, -0.5,       // Titik C
-    -0.5, 0.5      // Titik A
+    0.5, -0.5,      // Titik C
+    -0.5, -0.5       // Titik D
   ];
 
   // Deklarasi pointer
@@ -72,7 +72,7 @@ function main() {
   gl.clearColor(0.0, 0.5, 0.9, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var primitive = gl.TRIANGLE_STRIP;
+  var primitive = gl.TRIANGLE_FAN;
   var offset = 0;
   var nVertex = 4;
   gl.drawArrays(primitive, offset, nVertex);
